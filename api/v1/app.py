574a9837fe08d0855f1b8acd4d661944003b442e
yene2024 <yeneishla1524@gmail.com>
2024-04-19 13:10:46 -0400

    Fixing documentation

--- api/v1/app.py
+++ api/v1/app.py
@@ -5,6 +5,9 @@ from models import storage
 from api.v1.views import app_views
 import os
 
+"""
+    Run the Flask server.
+"""
 app = Flask(__name__)
 
 # Register the blueprint app_views to your Flask instance app
@@ -23,9 +26,6 @@ def teardown_db(exception):
 
 
 if __name__ == "__main__":
-    """
-    Run the Flask server.
-    """
     app.run(host=os.getenv('HBNB_API_HOST', '0.0.0.0'),
             port=int(os.getenv('HBNB_API_PORT', 5000)),
             threaded=True)
