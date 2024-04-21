d1a0803d3c4412fa3eaa3e574de80b93b727507b
yene2024 <yeneishla1524@gmail.com>
2024-04-21 15:14:45 -0400

    Updating api/v1/app.py to create a CORS instance allowing: /* for 0.0.0.0

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,39 +1,36 @@
 #!/usr/bin/python3
-
-"""
-Creating a handler for 404 errors
-that returns a JSON-formatted
-"""
-
+"""Contains the app object."""
+from flask import Flask, make_response, jsonify
 from models import storage
 from api.v1.views import app_views
-from os import environ
-from flask import Flask, jsonify, make_response
+from flask_cors import CORS
 
 app = Flask(__name__)
 app.url_map.strict_slashes = False
 app.config['JSONIFY_PRETTYPRINT_REGULAR'] = True
 app.register_blueprint(app_views)
+cors = CORS(app, resources={r"/api/v1/*": {"origins": "0.0.0.0"}})
 
 
 @app.teardown_appcontext
-def close_db(error):
-    """ Close Storage """
+def close_storage(exception):
+    """Close SQLAlchemy session."""
     storage.close()
 
 
 @app.errorhandler(404)
-def error_404(error):
-    """ gives json 404 error """
-    return make_response(jsonify({"error": "Not found"}), 404)
+def not_found(error):
+    """ 404 Error
+    ---
+    responses:
+      404:
+        description: a resource was not found
+    """
+    return make_response(jsonify({'error': "Not found"}), 404)
 
 
 if __name__ == "__main__":
-    """ Connects to host """
-    host = environ.get('HBNB_API_HOST')
-    port = environ.get('HBNB_API_PORT')
-    if not host:
-        host = '0.0.0.0'
-    if not port:
-        port = '5000'
+    import os
+    host = os.getenv('HBNB_API_HOST', '0.0.0.0')
+    port = os.getenv('HBNB_API_PORT', '5000')
     app.run(host=host, port=port, threaded=True)
