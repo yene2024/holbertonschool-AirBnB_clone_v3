7dcadc598038accb8e994e6bcfed62f61cb21653
yene2024 <yeneishla1524@gmail.com>
2024-04-21 12:54:29 -0400

    fixing code

--- api/v1/app.py
+++ api/v1/app.py
@@ -5,21 +5,35 @@ Creating a handler for 404 errors
 that returns a JSON-formatted
 """
 
-from flask import Flask, jsonify
+from models import storage
 from api.v1.views import app_views
+from os import environ
+from flask import Flask, jsonify, make_response
 
 app = Flask(__name__)
-
-# Registrar las rutas de la aplicación
+app.url_map.strict_slashes = False
+app.config['JSONIFY_PRETTYPRINT_REGULAR'] = True
 app.register_blueprint(app_views)
 
 
-# Manejador de errores 404
+@app.teardown_appcontext
+def close_db(error):
+    """ Close Storage """
+    storage.close()
+
+
 @app.errorhandler(404)
-def not_found(error):
-    """Handler for 404 errors."""
-    return jsonify({"error": "Not found"}), 404
+def error_404(error):
+    """ gives json 404 error """
+    return make_response(jsonify({"error": "Not found"}), 404)
 
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=5000)
+    """ Connects to host """
+    host = environ.get('HBNB_API_HOST')
+    port = environ.get('HBNB_API_PORT')
+    if not host:
+        host = '0.0.0.0'
+    if not port:
+        port = '5000'
+    app.run(host=host, port=port, threaded=True)
